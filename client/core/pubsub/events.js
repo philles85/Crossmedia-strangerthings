8376a730f564f0a3ba72e2b594c28bdc4fcc3593
philles85 <philster04@outlook.com>
2026-04-14 09:42:40 +0200

    Structuring events

--- client/core/pubsub/events.js
+++ client/core/pubsub/events.js
@@ -1,4 +1,40 @@
-const events = {
+const EVENTS = {
+
+    VIEWS: {
+        PAGE: {
+            STARTPAGE: "views:page:start",
+
+            GAMETIME: "views:page:gameplay",
+
+            NAVIGATION: "views:page:navigation",
+
+            ENDING: "views:page:ending"
+
+        },
+
+        POPUP: {
+
+        }
+
+    },
+
+    GAMESYSTEMS: {
+        TIMER: {
+            START: "gamesystems:timer:start",
+
+            MIDGAME: "gamesystems:timer:midgame",
+
+            END: "gamesystems:timer:end"
+        },
+
+        NAVIGATOR: {
+            
+
+        }
+
+
+
+    }
 
 
 
