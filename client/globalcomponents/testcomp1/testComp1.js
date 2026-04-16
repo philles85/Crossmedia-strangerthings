9b818ea64c12ffdf93b7c863b6a59c92fdace2b9
klaraassvensson <klara.svensson04@gmail.com>
2026-04-16 15:16:17 +0200

    header Text update

--- client/globalcomponents/testcomp1/testComp1.js
+++ client/globalcomponents/testcomp1/testComp1.js
@@ -20,9 +20,12 @@ class HeaderComp extends HTMLElement {
                     font-family: "Benguiat_outlined";
                     src: url("../fonts/Stranger_Things_Outlined.ttf") format("Outlined");
                 }
+                #header{
+                    text-align: center;
+                }
                 h1{
                     font-family: "Benguiat_outlined";
-                    color: #A10303;
+                    -webkit-text-stroke: 1px #A10303;
                     font-size: 48px;
                 }
                 h2{
@@ -31,7 +34,7 @@ class HeaderComp extends HTMLElement {
                     font-size: 24px;
                 }
             </style>
-            <div>
+            <div id="header">
                 <h1>STRANGER THINGS</h1>
                 <h2>THE LAST GATE</h2>
             </div>
