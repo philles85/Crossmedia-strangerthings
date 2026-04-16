3f893651379cb86a08696aed7aa0d7d8a1ac43ac
philles85 <philster04@outlook.com>
2026-04-16 15:12:58 +0200

    Mini server for frontend

--- client/views/startpage/components/StartPageTerminal.js
+++ client/views/startpage/components/StartPageTerminal.js
@@ -9,11 +9,47 @@ class StartPageTerminal extends HTMLElement {
 
     }
 
+    eventListeners() {
+        let inputField = this.shadowRoot.querySelector("input");
+
+        inputField.addEventListener("keydown", (event) => {
+            if (event.key == "enter") {
+                if (inputField.value == "") { }
+            }
+        })
+
+    }
+
     render() {
         this.shadowRoot.innerHTML = `
             <style>
-                
+
             </style>
+
+            <div id="startText">
+                <p>cd ..</p>
+                <p>cd hidden</p>
+                <p>access.log</p>
+                <p>nodes_04</p>
+                <p>signal.tmp</p>
+            </div>
+            
+            <div id="commandPrompt">
+                <p>run</p>
+                <input type="text">
+
+            </div>
+
+            <div id="responseText">
+                <p>WARNING:</p>
+                <p>if you choose to enter, you will not be allowed to leave this website.</p>
+            </div>
+
+            <div id="proceedPart">
+                <p>proceed? [enter]</p>
+                <button>enter</button>
+            </div>
+
         
         `
     }
