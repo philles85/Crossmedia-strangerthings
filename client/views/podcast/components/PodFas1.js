1ca7f448f1530cfebab3d27fa9f6af8de4031bc9
klaraassvensson <klara.svensson04@gmail.com>
2026-04-23 14:37:18 +0200

    Soundwaves works

--- client/views/podcast/components/PodFas1.js
+++ client/views/podcast/components/PodFas1.js
@@ -11,19 +11,59 @@ class PodFas1 extends HTMLElement {
 
     }
     d3_logic() {
-        const hSvg = 70, wSvg = 200;
-        const svg = d3.select("svg")
+        const hSvg = 70, wSvg = 350;
+        const svg = d3.select(this.shadowRoot)
+                    .select("svg")
                     .attr("height", hSvg)
-                    .attr("width", wSvg)
-        console.log(svg)
-        let randomHeight = Math.floor(10 * Math.random())
-        console.log(randomHeight)
-        for (let i = 0; i < 10; i++) {
-            console.log(i)
-            svg.append("rect")
-                .attr("height", randomHeight)
-                .attr("width", 10)
-        }
+                    .attr("width", wSvg);
+
+        let rectArray = [];
+        for (let i = 0; i < 50; i++) {
+            let randomHeight = Math.floor(70 * Math.random())
+            rectArray.push({id: i, height: randomHeight})
+        } 
+        
+        let xScale = d3.scaleBand(rectArray, [0, wSvg])
+                        .paddingInner(.8)
+                        .paddingOuter(.9);
+
+        let testArray = svg.append("g")
+            .selectAll("rect")
+            .data(rectArray)
+            .enter()
+            .append("rect")
+            .attr("height", d => d.height)
+            .attr("width", xScale.bandwidth())
+            .attr("x", d => xScale(d))
+            .attr("y", d => (hSvg - d.height) / 2)
+            .attr("fill", "darkgrey")
+                
+
+        let allRects = testArray.nodes()
+        let timer = null;
+        const audioDOM = this.shadowRoot.querySelector("audio");
+        let storeState = 0;
+
+        audioDOM.addEventListener("play", function (){
+            timer = setInterval(function(){
+                if (storeState >= allRects.length){
+                    clearInterval(timer);
+                    timer = 0;
+                    return;
+                }
+                let node = allRects[storeState]
+                d3.select(node).attr("fill", "white")
+                storeState++
+            }, 360)
+
+        })
+
+        audioDOM.addEventListener("pause", function (){
+            clearInterval(timer);
+            timer = 0;
+            return;            
+        })
+
     }
 
     render() {
