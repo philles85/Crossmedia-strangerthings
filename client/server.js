570b97375c92fbe0ba668c2df69371150dbbff7d
philles85 <philster04@outlook.com>
2026-05-04 10:26:17 +0200

    Maybe working

--- client/server.js
+++ client/server.js
@@ -2,14 +2,14 @@ import { serveDir } from "https://deno.land/std/http/file_server.ts";
 
 async function handler(request) {
     const response = await serveDir(request, {
-        fsRoot: "./client",
+        fsRoot: ".",
         urlRoot: "",
         showDirListing: true
     })
 
     if (response.status == 404) {
         return serveDir(new Request(new URL("/index.html", request.url)), {
-            fsRoot: "./client"
+            fsRoot: "."
         })
     }
     return response;
@@ -17,6 +17,6 @@ async function handler(request) {
 }
 
 Deno.serve({
-    port: Number(Deno.env.get("PORT")) || 8080,
+    port: Number(Deno.env.get("PORT")) || 8000,
     hostname: "0.0.0.0"
 }, handler);
