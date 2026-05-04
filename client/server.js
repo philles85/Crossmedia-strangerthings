827061497d6ee6d62f8cec38d7fb212f7b5d55d9
philles85 <philster04@outlook.com>
2026-05-04 10:52:25 +0200

    Update

--- client/server.js
+++ client/server.js
@@ -1,5 +1,6 @@
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
+console.log("STARTING SERVER");
+console.log("PORT:", Deno.env.get("PORT"));
 async function handler(request) {
     const response = await serveDir(request, {
         fsRoot: ".",
@@ -9,7 +10,8 @@ async function handler(request) {
 
     if (response.status == 404) {
         return serveDir(new Request(new URL("/index.html", request.url)), {
-            fsRoot: "."
+            fsRoot: ".",
+            urlRoot: ""
         })
     }
     return response;
