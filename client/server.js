d80ffade96f08e7dd8aa7a5a5df3d38e98fde785
philles85 <philster04@outlook.com>
2026-05-04 10:12:15 +0200

    .

--- client/server.js
+++ client/server.js
@@ -17,6 +17,6 @@ async function handler(request) {
 }
 
 Deno.serve({
-    port: Number(Deno.env.get("PORT")) || 8000,
+    port: Number(Deno.env.get("PORT")) || 8080,
     hostname: "0.0.0.0"
 }, handler);
